60e7f0264e7af61fb9cd4fed6b169d1ec63c10e5
Tobias Meyer <tobias@hashicorp.com>
2022-08-04 10:58:25 +0200

    Remove unused constants
    Capabilities can be identified by any string and do need to be defined
    in the library.

--- internal/capability/capability.go
+++ internal/capability/capability.go
@@ -4,14 +4,6 @@ import (
 	"net"
 )
 
-const (
-	// CapabilityGRPC is the capability to call a gRPC server.
-	CapabilityGRPC = "gRPC"
-
-	// CapabilityHTTP is the capability to call an HTTP server.
-	CapabilityHTTP = "HTTP"
-)
-
 // Provider is used to provide a given capability when requested remotely. They
 // must return a connection that is bridged or an error.
 type Provider func(capability string, meta map[string]string, conn net.Conn) error
