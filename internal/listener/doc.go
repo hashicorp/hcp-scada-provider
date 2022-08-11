d1ba71f6d654794541c7d3744686d61826a38290
Dominic Gregoire <dominic.gregoire@hashicorp.com>
2022-08-11 09:44:53 -0400

    moving the listener package godoc to listener.go

--- internal/listener/doc.go
+++
@@ -1,4 +0,0 @@
-// listener is a capability that pushes the received connection into a
-// net.Listener. This capability allows exposing services that wrap a
-// listener such as gRPC or HTTP Servers very easily.
-package listener
--- internal/listener/listener.go
+++ internal/listener/listener.go
@@ -1,3 +1,6 @@
+// listener is a capability that pushes the received connection into a
+// net.Listener. This capability allows exposing services that wrap a
+// listener such as gRPC or HTTP Servers very easily.
 package listener
 
 import (
